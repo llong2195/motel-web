8aac650215b6b3b507aea079102b9e5eaa31aeeb
Nguyen Duy Long <68091468+llong2195@users.noreply.github.com>
2023-05-31 16:58:03 +0700

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://test-api-1-production.up.railway.app' }); // http://103.166.183.203:3000/
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
